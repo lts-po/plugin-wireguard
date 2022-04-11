341b9e88f09c6276853aefc0bd2a9f30646cbe35
lts-po <philip.olausson@longterm.io>
2022-04-11 14:52:36 +0200

    export LANIP for endpoint

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 . /configs/base/config.sh
 # haxxy way to put the pubkey in env for go instead of parsing the conf
+export LANIP=$LANIP
 export WIREGUARD_NETWORK=$WIREGUARD_NETWORK
 export WIREGUARD_PORT=$WIREGUARD_PORT
 WIREGUARD_PRIVKEY=$(cat /configs/wireguard/wg0.conf|grep -m1 ^PrivateKey | sed 's/\s//g'|sed 's/PrivateKey=//g')
